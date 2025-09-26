40461ace6546184d6901b096004860ff7bb05673
Cristian Prochnow <cristianprochnow@gmail.com>
2025-09-26 00:29:43 -0300

    fix: fix CORS

--- app/controller/controller.go
+++ app/controller/controller.go
@@ -1,5 +1,7 @@
 package controller
 
+import "net/http"
+
 type Response struct {
 	Ok    bool   `json:"ok"`
 	Error string `json:"error"`
@@ -10,4 +12,12 @@ type Response struct {
 type ResponseHeader struct {
 	Ok    bool   `json:"ok"`
 	Error string `json:"error"`
+}
+
+func EnableCors(w http.ResponseWriter) http.ResponseWriter {
+	w.Header().Set("Access-Control-Allow-Origin", "*")
+	w.Header().Set("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS")
+	w.Header().Set("Access-Control-Allow-Headers", "Content-Type, Authorization")
+
+	return w
 }
