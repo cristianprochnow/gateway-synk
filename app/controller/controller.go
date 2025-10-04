7a61c3f8ca4100c2c084e0ca4c07b962870902e3
Cristian Prochnow <cristianprochnow@gmail.com>
2025-10-03 21:12:27 -0300

    feat(controller): add generic methods to response

--- app/controller/controller.go
+++ app/controller/controller.go
@@ -1,6 +1,10 @@
 package controller
 
-import "net/http"
+import (
+	"encoding/json"
+	"net/http"
+	"synk/gateway/app/util"
+)
 
 type Response struct {
 	Ok    bool   `json:"ok"`
@@ -14,10 +18,28 @@ type ResponseHeader struct {
 	Error string `json:"error"`
 }
 
-func EnableCors(w http.ResponseWriter) http.ResponseWriter {
+func EnableCors(w http.ResponseWriter) {
 	w.Header().Set("Access-Control-Allow-Origin", "*")
 	w.Header().Set("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS")
 	w.Header().Set("Access-Control-Allow-Headers", "Content-Type, Authorization")
+}
 
-	return w
+func WriteErrorResponse(w http.ResponseWriter, response any, route string, message string, status int) {
+	util.LogRoute(route, message)
+
+	jsonResp, _ := json.Marshal(response)
+
+	w.WriteHeader(status)
+	w.Write(jsonResp)
+}
+
+func WriteSuccessResponse(w http.ResponseWriter, response any) {
+	jsonResp, _ := json.Marshal(response)
+
+	w.WriteHeader(http.StatusOK)
+	w.Write(jsonResp)
+}
+
+func SetJsonContentType(w http.ResponseWriter) {
+	w.Header().Set("Content-Type", "application/json")
 }
