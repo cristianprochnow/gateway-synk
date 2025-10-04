4573e62d644c05c2e45032c3b537f68efe05b67e
Cristian Prochnow <cristianprochnow@gmail.com>
2025-10-03 21:52:56 -0300

    feat(controller): implement update method to Post

--- app/controller/posts.go
+++ app/controller/posts.go
@@ -28,13 +28,30 @@ type HandlePostCreateRequest struct {
 	IntProfileId int    `json:"int_profile_id"`
 }
 
+type HandlePostUpdateRequest struct {
+	PostId       int    `json:"post_id"`
+	PostName     string `json:"post_name"`
+	PostContent  string `json:"post_content"`
+	TemplateId   int    `json:"template_id"`
+	IntProfileId int    `json:"int_profile_id"`
+}
+
 type HandlePostCreateResponse struct {
 	Resource ResponseHeader               `json:"resource"`
 	Data     CreatePostCreateDataResponse `json:"post"`
 }
 
+type HandlePostUpdateResponse struct {
+	Resource ResponseHeader         `json:"resource"`
+	Data     UpdatePostDataResponse `json:"post"`
+}
+
 type CreatePostCreateDataResponse struct {
 	PostId int `json:"post_id"`
+}
+
+type UpdatePostDataResponse struct {
+	RowsAffected int `json:"rows_affected"`
 }
 
 func NewPosts(db *sql.DB) *Posts {
@@ -163,6 +180,114 @@ func (p *Posts) HandleCreate(w http.ResponseWriter, r *http.Request) {
 	}
 
 	response.Data.PostId = creationId
+
+	WriteSuccessResponse(w, response)
+}
+
+func (p *Posts) HandleUpdate(w http.ResponseWriter, r *http.Request) {
+	EnableCors(w)
+	SetJsonContentType(w)
+
+	response := HandlePostUpdateResponse{
+		Resource: ResponseHeader{
+			Ok: true,
+		},
+		Data: UpdatePostDataResponse{},
+	}
+
+	bodyContent, bodyErr := io.ReadAll(r.Body)
+
+	if bodyErr != nil {
+		response.Resource.Ok = false
+		response.Resource.Error = "error on read update body"
+
+		WriteErrorResponse(w, response, "/posts", response.Resource.Error, http.StatusBadRequest)
+
+		return
+	}
+
+	var post HandlePostUpdateRequest
+
+	jsonErr := json.Unmarshal(bodyContent, &post)
+
+	if jsonErr != nil {
+		response.Resource.Ok = false
+		response.Resource.Error = "some fields can be in invalid format"
+
+		WriteErrorResponse(w, response, "/posts", response.Resource.Error, http.StatusBadRequest)
+
+		return
+	}
+
+	post.PostName = strings.TrimSpace(post.PostName)
+	post.PostContent = strings.TrimSpace(post.PostContent)
+
+	hasAllData := post.PostId != 0 &&
+		post.PostName != "" &&
+		post.PostContent != "" &&
+		post.TemplateId != 0 &&
+		post.IntProfileId != 0
+
+	if !hasAllData {
+		response.Resource.Ok = false
+		response.Resource.Error = "fields post_id, post_name, post_content, template_id, int_profile_id are required"
+
+		WriteErrorResponse(w, response, "/posts", response.Resource.Error, http.StatusBadRequest)
+
+		return
+	}
+
+	postById, _ := p.model.ById(post.PostId)
+
+	if postById.PostId == 0 {
+		response.Resource.Ok = false
+		response.Resource.Error = "post with id " + strconv.Itoa(post.PostId) + " not found"
+
+		WriteErrorResponse(w, response, "/posts", response.Resource.Error, http.StatusBadRequest)
+
+		return
+	}
+
+	templateById, _ := p.templateModel.ById(post.TemplateId)
+
+	if templateById.TemplateId == 0 {
+		response.Resource.Ok = false
+		response.Resource.Error = "template with id " + strconv.Itoa(post.TemplateId) + " not found"
+
+		WriteErrorResponse(w, response, "/posts", response.Resource.Error, http.StatusBadRequest)
+
+		return
+	}
+
+	intProfileById, _ := p.intProfileModel.ById(post.IntProfileId)
+
+	if intProfileById.IntProfileId == 0 {
+		response.Resource.Ok = false
+		response.Resource.Error = "integration profile with id " + strconv.Itoa(post.IntProfileId) + " not found"
+
+		WriteErrorResponse(w, response, "/posts", response.Resource.Error, http.StatusBadRequest)
+
+		return
+	}
+
+	rowsAffected, updateErr := p.model.Update(model.PostUpdateData{
+		PostId:       post.PostId,
+		PostName:     post.PostName,
+		PostContent:  post.PostContent,
+		TemplateId:   post.TemplateId,
+		IntProfileId: post.IntProfileId,
+	})
+
+	if updateErr != nil {
+		response.Resource.Ok = false
+		response.Resource.Error = updateErr.Error()
+
+		WriteErrorResponse(w, response, "/posts", response.Resource.Error, http.StatusBadRequest)
+
+		return
+	}
+
+	response.Data.RowsAffected = rowsAffected
 
 	WriteSuccessResponse(w, response)
 }
