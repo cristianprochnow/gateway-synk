234a999778365f93fa534ed275fd1ba7b0336dfd
Cristian Prochnow <cristianprochnow@gmail.com>
2025-10-06 18:36:38 -0300

    feat(controller): add method to handle CORS

--- app/controller/controller.go
+++ app/controller/controller.go
@@ -43,3 +43,18 @@ func WriteSuccessResponse(w http.ResponseWriter, response any) {
 func SetJsonContentType(w http.ResponseWriter) {
 	w.Header().Set("Content-Type", "application/json")
 }
+
+func Cors(next http.Handler) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		w.Header().Set("Access-Control-Allow-Origin", "*")
+		w.Header().Set("Access-Control-Allow-Methods", "POST, GET, OPTIONS, PUT, DELETE")
+		w.Header().Set("Access-Control-Allow-Headers", "Accept, Content-Type, Content-Length, Accept-Encoding, X-CSRF-Token, Authorization")
+
+		if r.Method == "OPTIONS" {
+			w.WriteHeader(http.StatusNoContent)
+			return
+		}
+
+		next.ServeHTTP(w, r)
+	})
+}
