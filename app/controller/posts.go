f611d457fefcdfe56479cff29abb1b239372e0fa
Cristian Prochnow <cristianprochnow@gmail.com>
2025-10-07 00:57:20 -0300

    feat(controller): implement param to also get post content

--- app/controller/posts.go
+++ app/controller/posts.go
@@ -72,8 +72,9 @@ func (p *Posts) HandleList(w http.ResponseWriter, r *http.Request) {
 	SetJsonContentType(w)
 
 	postId := r.URL.Query().Get("post_id")
+	includeContent := r.URL.Query().Get("include_content")
 
-	postList, postErr := p.model.List(postId)
+	postList, postErr := p.model.List(postId, includeContent == "1")
 
 	response := HandleListResponse{
 		Resource: ResponseHeader{
